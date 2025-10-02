8be18932429b53d2c925ae7e6320dba6abdf7dfc
bnjax3 <153530323+bnjax3@users.noreply.github.com>
2025-10-01 23:29:12 -0300

    estudiar sistemas o hacerse el wey
    la concha de la mama del que hizo github desktop, anda bien ,al gitignore

---
+++ src/main/java/org/bnjax3/noitacraft/other/Utils.java
@@ -0,0 +1,34 @@
+package org.bnjax3.noitacraft.other;
+
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+
+public abstract class Utils {
+    // shit name i know
+    public static String addCharUntilLenghtX(String string, int lenght, char filler){
+        String toReturn = string;
+        for (int i = 0;  i < lenght - string.length(); i++){
+            toReturn += filler;
+        }
+        return toReturn;
+    }
+    public static TranslationTextComponent addCharUntilLenghtX(TranslationTextComponent textComponent, int lenght, char filler){
+        for (int i = 0;  i < lenght - textComponent.getString().length(); i++){
+            textComponent.append(String.valueOf(filler));
+        }
+        return textComponent;
+    }
+    public static TranslationTextComponent FormatTooltipData(String resourceLocation, boolean data){
+        TranslationTextComponent text = new TranslationTextComponent(resourceLocation);
+        return (TranslationTextComponent) addCharUntilLenghtX(text, 20, '-').append(String.valueOf(data));
+    }
+    public static TranslationTextComponent FormatTooltipData(String resourceLocation, int data){
+        TranslationTextComponent text = new TranslationTextComponent(resourceLocation);
+        return (TranslationTextComponent) addCharUntilLenghtX(text, 20, '-').append(String.valueOf(data));
+    }
+    public static TranslationTextComponent FormatTooltipData(String resourceLocation, float data){
+        TranslationTextComponent text = new TranslationTextComponent(resourceLocation);
+        return (TranslationTextComponent) addCharUntilLenghtX(text, 20, '-').append(String.valueOf(data));
+    }
+}
--- src/main/java/org/bnjax3/noitacraft/wand/WandItem.java
+++ src/main/java/org/bnjax3/noitacraft/wand/WandItem.java
@@ -1,6 +1,8 @@
 package org.bnjax3.noitacraft.wand;
 
 import mcp.MethodsReturnNonnullByDefault;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -8,10 +10,20 @@ import net.minecraft.item.ItemUseContext;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
+import net.minecraft.util.text.IFormattableTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.loading.progress.StartupMessageManager;
+import org.apache.logging.log4j.core.config.builder.api.Component;
+import org.apache.logging.log4j.message.Message;
+import org.bnjax3.noitacraft.other.Utils;
 import org.bnjax3.noitacraft.spell.Spell;
 
+import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.awt.*;
+import java.util.List;
 
 public class WandItem extends Item {
 
@@ -21,6 +33,35 @@ public class WandItem extends Item {
     public WandItem(Properties itemProperties, Wand wand) {
         super(itemProperties);
         Wand1 = wand;
+    }
+    /*
+    public final boolean Shuffle;
+    public final int SpellsCast;
+    public final int ManaMax;
+    public final int Capacity;
+    public final int ManaChargeSpeed;
+    public final int CastDelay; // in ticks
+    public final float Spread; // degrees
+    public final float SpeedMult;
+    public final int RechargeTime; // in ticks
+     */
+    @Override
+    public void appendHoverText(ItemStack itemStack, @Nullable World world, List<ITextComponent> textComponents, ITooltipFlag tooltipFlag) {
+        if (Screen.hasShiftDown()){
+            // es un chingo de texto
+            textComponents.add(Utils.FormatTooltipData("tooltip.noitacraft.shuffle", this.Wand1.Shuffle));
+            textComponents.add(Utils.FormatTooltipData("tooltip.noitacraft.spellsCast", this.Wand1.SpellsCast));
+            textComponents.add(Utils.FormatTooltipData("tooltip.noitacraft.castDelay", this.Wand1.CastDelay));
+            textComponents.add(Utils.FormatTooltipData("tooltip.noitacraft.rechargeTime", this.Wand1.RechargeTime));
+            textComponents.add(Utils.FormatTooltipData("tooltip.noitacraft.manaMax", this.Wand1.ManaMax));
+            textComponents.add(Utils.FormatTooltipData("tooltip.noitacraft.manaChargeSpeed", this.Wand1.ManaChargeSpeed));
+            textComponents.add(Utils.FormatTooltipData("tooltip.noitacraft.capacity", this.Wand1.Capacity));
+            textComponents.add(Utils.FormatTooltipData("tooltip.noitacraft.spread", this.Wand1.Spread));
+            textComponents.add(Utils.FormatTooltipData("tooltip.noitacraft.speedMult", this.Wand1.SpeedMult));
+        } else {
+            textComponents.add(new TranslationTextComponent("tooltip.noitacraft.shiftForDetail"));
+        }
+        super.appendHoverText(itemStack, world, textComponents, tooltipFlag);
     }
 
     @Override
